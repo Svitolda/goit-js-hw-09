10ad58b1a5caaa66552e73c021c4ed4a7eb4b079
Svitolda <sveta20000203@gmail.com>
2023-04-20 23:25:52 +0200

    01upgrade

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,25 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]'); 
+
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+
+startBtn.disabled = false; 
+
+function onStartBtn() {
+  intervalId = setInterval(changeBackgroundColor, 1000);
+  startBtn.disabled = true;
+} 
+
+function onStopBtn() {
+  clearInterval(intervalId);
+}
+
+function changeBackgroundColor() {
+        const color = getRandomHexColor();
+        document.body.style.backgroundColor = color;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}

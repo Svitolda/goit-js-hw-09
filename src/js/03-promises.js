11f0f407f7137307784bd08f07fbf2f3b79225f8
Svitolda <sveta20000203@gmail.com>
2023-04-20 23:36:53 +0200

    03upgrade

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import Notiflix from 'notiflix';
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, delay);
+  });
+}
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onBtnSubmitClick);
+
+function onBtnSubmitClick(evt) {
+  evt.preventDefault();
+
+  let delay = Number(evt.target.elements.delay.value);
+  const step = Number(evt.target.elements.step.value);
+  const amount = Number(evt.target.elements.amount.value);
+
+  for (let i = 0; i < amount; i++) {
+    createPromise(i, delay);
+    delay += step;
+  };
+
+  if (amount === undefined) {
+    Notiflix.Notify.warning('Please fill in all fields');
+    return;
+  };
+
+  createPromise(2, 1500)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
 }
